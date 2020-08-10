dbe4cccb95c448acdd181de1fef4adc2f685121c
anurag533 <anuragsharma.7896@gmail.com>
2020-08-10 16:35:38 -0400

    changes

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -1,6 +1,11 @@
 package 'ntp'
 
 file '/etc/motd' do
-content 'this is the property of anurag'
+content "this is the property of anurag
+HOSTNAME: #{node['hostname']}
+IPADDRESS: #{node['ipaddress']}
+CPU: #{node['cpu']['0']['mhz']}
+MEMORY: #{node['memory']['total']}
+"
 action :create
 end
